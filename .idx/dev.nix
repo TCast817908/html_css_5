32c70bc8446407a10f06cfdb0ef01053875a7cce
ttillo1821 <ttillo1821@gmail.com>
2025-09-15 12:13:11 +0000

    assignment01

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,9 +7,9 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-     pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
-     pkgs.nodejs_20
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
 
@@ -25,17 +25,17 @@
     previews = {
       enable = true;
       previews = {
-         web = {
+        web = {
         #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
         #   # and show it in IDX's web preview panel
-           command = ["npm" "run" "dev"];
-           command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-           manager = "web";
+        # command = ["npm" "run" "dev"];
+        command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+         manager = "web";
         #   env = {
         #     # Environment variables to set for your server
         #     PORT = "$PORT";
         #   };
-         };
+        };
       };
     };
 
@@ -45,7 +45,8 @@
       onCreate = {
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
-      default.openFiles = [ "style.css" "main.js" "index.html"};
+        default.openFiles = [ "style.css" "main.js" "index.html" ];
+      };
       # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
